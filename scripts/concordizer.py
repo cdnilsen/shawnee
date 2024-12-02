3fa9ebf6cfa4cda77312833827eb70e2969495ec
cdnilsen <campbell.nilsen@gmail.com>
2024-12-01 19:50:17 -0500

    added 168; started getting word count

--- scripts/concordizer.py
+++ scripts/concordizer.py
@@ -1,3 +1,5 @@
+import os
+
 alfordList = open('../alfordWordsPlain.txt', 'r', encoding='utf-8').readlines()
 
 def processAlfordWord(line):
@@ -27,3 +29,42 @@ allAlfordLinesList = []
 
 for line in alfordList:
     allAlfordLinesList.append(processAlfordWord(line))
+
+def processTextLine(line, dict, count):
+    if "\\\\" in line:
+        word = line.split("\\\\")[0].strip()
+        spuriousChars = ["[", "]", "(", ")", "{", "}"]
+        for char in spuriousChars:
+            word = word.replace(char, "")
+        if word not in dict:
+            dict[word] = 1
+        else:
+            dict[word] += 1
+        return True
+    else:
+        return False
+
+def getAllTextFiles():
+    masterDict = {}
+    textFileHandle = "../texts"
+    allWordCount = 0
+    for entry in os.listdir(textFileHandle):
+        subFolder= textFileHandle + "/" + entry
+        for entry in os.listdir(subFolder):
+            fileLines = open(subFolder + "/" + entry, "r", encoding="utf-8").readlines()
+            for line in fileLines:
+                if processTextLine(line, masterDict, allWordCount):
+                    allWordCount += 1
+
+    allWords = list(masterDict.keys())
+
+    hapaxCount = 0
+    for word in allWords:
+        if masterDict[word] == 1:
+            hapaxCount += 1
+
+    print(str(hapaxCount) + " hapaxes")
+    print(str(len(allWords)) + " total words")
+    print(str(allWordCount) + " total tokens")
+
+getAllTextFiles()
